346867f977ee12d1b0160c8e42efe98ed25d9b3d
Chitrangi Mestry <chitrangi.mestry@softlabsgroup.com>
2025-07-17 18:38:32 +0530

    Added new file

---
+++ Day_09/main.py
@@ -0,0 +1,8 @@
+#Ask the user for the inputs
+name = input("What is your name? : ")
+bid = input("What is your bid? : $")
+#Save the data to the dictionary
+
+# Whether the new bids need to be added
+
+# Compare the bids in the dictionary
--- Day_09/task.py
+++ Day_09/task.py
@@ -3,26 +3,44 @@ programming_dictionary = {"bug": "An Error that prevents programs from running."
                           "loop": "An action of doing something over and over again"
                          }
 
-print(programming_dictionary["bug"])
+# print(programming_dictionary["bug"])
 
 programming_dictionary["middleware"] = "A function written to provide security layer before accessing any route."
 
-print(programming_dictionary)
+# print(programming_dictionary)
 
 #loop
-for key in programming_dictionary:
-    print(key)
-    print(key + ": " + programming_dictionary[key])
+# for key in programming_dictionary:
+#     print(key)
+#     print(key + ": " + programming_dictionary[key])
 
 
-captials = {
+capitals = {
     "France" : "Paris",
     "Germany" : "Berlin",
     "Netherlands": "Amsterdam",
     "Sweden": "Stockholm"
 }
 
+# travel_log = {
+#     "France": ["Île-de-France", "Notre-Dame", "Louvre", "Basillique du Sacre-Cœour de Montmartre"],
+#     "Germany": ["Berlin", "Munich", "Hamburg", "Frankfurt am Main", "Stuttgrat"]
+# }
+
+# print(travel_log["Germany"][1])
+
+nested_list =["A", "B", ["C", "D"]];
+print(nested_list[2][0])
+
 travel_log = {
-    "France": ["Île-de-France", "Notre-Dame", "Louvre", "Basillique du Sacre-Cœour de Montmartre"],
-    "Germany": ["Berlin", "Munich", "Hamburg", "Frankfurt am Main", "Stuttgrat"]
+    "France": {
+        "num_times_visited" : 8,
+        "cities_visited" : ["Île-de-France", "Notre-Dame", "Louvre", "Basillique du Sacre-Cœour de Montmartre"]
+    },
+    "Germany": {
+        "num_times_visited": 5,
+        "cities_visited" : ["Berlin", "Munich", "Hamburg", "Frankfurt am Main", "Stuttgrat"]
+    },
 }
+
+print(travel_log["Germany"]["cities_visited"][4])
